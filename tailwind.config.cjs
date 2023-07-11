157db917bc6c129715c455e4697ae8a7affbd117
Ed O'Connor-Giles <ed@economicsecurityplanning.com>
2023-07-10 21:13:03 -0400

    Update favicon, logo and text in splash

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
       colors: {
         primary: "var(--color-primary)",
         secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)"
       },
       textColor: {
         default: "var(--color-text)",
